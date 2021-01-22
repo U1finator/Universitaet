fb6c5cfb2d2261c92f9fc085ba9a87e4eac75339
U1finator <59577069+U1finator@users.noreply.github.com>
2021-01-22 13:49:10 +0100

    Update Interface.java

--- Interface/src/Interface/Interface.java
+++ Interface/src/Interface/Interface.java
@@ -1,15 +1,15 @@
 package Interface;
 public class Interface {
 	public static void main(String[] args) {
-		//eine Scannerabfrage für Name, Alter und Körpergröße
+		//eine Scannerabfrage fÃ¼r Name, Alter und KÃ¶rpergrÃ¶ÃŸe
 		System.out.println("Wie ist ihr Name??");
 		String name = new java.util.Scanner(System.in).nextLine();	//Scannerabfrage des Namens durch Userinput
 		System.out.println("Darf ich auch Ihr Alter erfahren??");
 		int age = new java.util.Scanner(System.in).nextInt();		//Scannerabrafe des Alters durch Userinput
-		System.out.println("Ich benoetige noch Ihre Koerpergroeße in m");
-		double hight = new java.util.Scanner(System.in).nextDouble();	//Scannerabfrage der Körpergröße durch Userinput
+		System.out.println("Ich benoetige noch Ihre KoerpergroeÃŸe in m");
+		double height = new java.util.Scanner(System.in).nextDouble();	//Scannerabfrage der KÃ¶rpergrÃ¶ÃŸe durch Userinput
 		
 		System.out.println("Ihr Name ist also: " + name + " und sie sind " + age + " Jahre alt"); //Ausgabe der eingegebenen Daten
-		System.out.print("Ihre Koerpergröße betraegt nach ihren Angaben: " + hight);
+		System.out.print("Ihre KoerpergrÃ¶ÃŸe betraegt nach ihren Angaben: " + height);
 	}
 }
