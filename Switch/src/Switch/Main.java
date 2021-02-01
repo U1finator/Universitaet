3cc28a287821b251c3aa59b3df6ab6720a326d8c
owern <owern@DESKTOP-MPHVF8N.fritz.box>
2021-02-01 09:03:20 +0100

    added comments

--- Switch/src/Switch/Main.java
+++ Switch/src/Switch/Main.java
@@ -4,16 +4,17 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Alter age = new Alter();
-		Steuer tax = new Steuer();
+		Scanner scan = new Scanner(System.in);		//neuer Scanner
+		Alter age = new Alter();		//neues Objekt der Klasse Alter
+		Steuer tax = new Steuer();		//neues Objekt der Klasse Steuer
 		
-		System.out.println("Wie hoch ist ihr Gehalt??");
-		int gehalt = scan.nextInt();
-		System.out.println("Ihre Steuern sind: " + tax.betrag(gehalt) + "€");
+		System.out.println("Wie hoch ist ihr Jahresgehalt??");		//Abfrage nach der Höhe des Gehaltes
+		int gehalt = scan.nextInt();		//Gehaltseingabe wird abgefragt
+		System.out.println("Ihre Steuern sind: " + tax.betrag(gehalt) + "€");		//Die Höhe der Steuern wird ausgegeben
 		
-		System.out.println("Wie alt sind Sie?");
-		int alter = scan.nextInt();
-		System.out.println(age.abfrage(alter));
+		System.out.println("Wie alt sind Sie?");	//Frage die man keiner Frau stellen darf
+		int alter = scan.nextInt();		//Abfrage der Usereingabe
+		System.out.println(age.abfrage(alter));		//Ausgabe ob Alter ein besonderes Alter ist oder nicht
+		scan.close();		//Ende der Usereingabeabfrage
 	}
 }
--- Switch/src/Switch/Steuer.java
+++ Switch/src/Switch/Steuer.java
@@ -3,24 +3,24 @@ package Switch;
 public class Steuer {
 	public double betrag(double gehalt)
 	{
-		double steuer;
-		if(gehalt <= 12000)
+		double steuer;		//Rückgabewert
+		if(gehalt <= 12000)		//Gehalt kleiner 12000
 		{
-			steuer = gehalt*0.12;
+			steuer = gehalt*0.12;	//Rückgabewert als Gehalt mal Steuersatz
 		}
-		else if(gehalt <= 20000)
+		else if(gehalt <= 20000)	//Gehalt bis einschließlich 20000 und größer als 12000
 		{
-			steuer = gehalt*0.15;
+			steuer = gehalt*0.15;	//Rückgabewert als Gehalt mal Steuersatz
 		}
-		else if(gehalt <= 30000)
+		else if(gehalt <= 30000)	//Gehalt bis einschließlich 30000und größer als 20000
 		{
-			steuer = gehalt*0.20;
+			steuer = gehalt*0.20;	//Rückgabewert als Gehalt mal Steuersatz
 		}
-		else if(gehalt > 30000)
+		else if(gehalt > 30000)		//Gehalt über 30000
 		{
-			steuer = gehalt*0.25;
+			steuer = gehalt*0.25;	//Rückgabewert als Gehalt mal Steuersatz
 		}
-		else
+		else						//Idiotenprävention
 		{
 			System.out.println("Invalid input");
 			steuer = 0;
