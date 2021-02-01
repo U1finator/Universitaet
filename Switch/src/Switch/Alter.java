7b37c7f4f645214b0a3c27a0bb28e4d1d00fae4a
owern <owern@DESKTOP-MPHVF8N.fritz.box>
2021-02-01 09:04:01 +0100

    added comments

--- Switch/src/Switch/Alter.java
+++ Switch/src/Switch/Alter.java
@@ -3,16 +3,16 @@ package Switch;
 public class Alter {
 	public String abfrage(int alter)
 	{
-		String back;
-		switch(alter)
+		String back;	//Rückgabe String
+		switch(alter)	//Switch-Case Abfrage nach Altern
 		{
-		case 16:
+		case 16:		//mit 16 Jahren
 			back = "Du darfst Mofa fahren und Bier trinken";
 			break;
-		case 18:
+		case 18:		//mit 18 Jahren
 			back = "Du bist volljährig";
 			break;
-		default:
+		default:		//sonstige Eingaben
 			back = "Das ist kein besonderes Alter";
 		}
 		return back;
