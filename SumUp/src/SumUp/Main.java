ca11f7f50e72551ae140039a5959cbcee4ff2816
U1finator <59577069+U1finator@users.noreply.github.com>
2021-02-05 09:10:43 +0100

    Update Main.java
    made an option to restart the process

--- SumUp/src/SumUp/Main.java
+++ SumUp/src/SumUp/Main.java
@@ -1,18 +1,24 @@
 package SumUp;
 import java.util.Scanner;
-public class Main {
-
+public class Main 
+{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);		//ein Scanner zur Abfrage der Eingabe
 		Sum sum = new Sum();		//neues Objekt der Klasse Sum
 		
-		System.out.println("Welche Zahl darf es sein?");
-		int number = scan.nextInt();	//Abfrage der Zahl
+		String again = "n";
+		do
+		{
+			System.out.println("Welche Zahl darf es sein?");
+			int number = scan.nextInt();	//Abfrage der Zahl
 		
-		System.out.println("Das Ergebnis der iterativen Methode lautet: " + sum.sum_iter(number));		//die Summenberechnung durch die iterative Methode
-		System.out.println("Das Ergebnis der rekursiven Methode lautet: " + sum.sum_recur(number));		//die Summenberechnung durch die rekursive Methode
+			System.out.println("Das Ergebnis der iterativen Methode lautet: " + sum.sum_iter(number));		//die Summenberechnung durch die iterative Methode
+			System.out.println("Das Ergebnis der rekursiven Methode lautet: " + sum.sum_recur(number));		//die Summenberechnung durch die rekursive Methode
+			
+			System.out.println("Zum Neustart 'ja' eingeben");
+			again = scan.next();		//Abfrage ob das Programm Neugestartet werden soll
+		}while(again == "ja");	//wenn die Eingabe 'ja' ist, wird das Programm von vorne gestartet
 		
-		scan.close();		//den Scanner schlieﬂen
+		scan.close();		//den Scanner schlie√üen
 	}
-
 }
