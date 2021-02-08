d7a95e6f5112e738f4f1791c623403a28ec5062a
U1finator <59577069+U1finator@users.noreply.github.com>
2021-02-08 13:28:14 +0100

    Update Main.java
    updated the Code

--- Player and Doors/src/playersAndDoors/Main.java
+++ Player and Doors/src/playersAndDoors/Main.java
@@ -8,34 +8,35 @@ public class Main
 	public static void main (String[] args)
 	{	
 		java.awt.Point player = new java.awt.Point();	//ein neuer Spieler wird erstellt
-		player.x = player.y = 0;	//Koordinaten des Spielers sind (0,0)
+		player.x = player.y = 1;	//Koordinaten des Spielers sind (1,1)
 		
 		Random rand = new Random();
 		
-		java.awt.Point door = new java.awt.Point();		//eine neue Tür zum Suchen wird erstellt
-		door.setLocation(rand.nextInt(10),  rand.nextInt(10));	//die Tür wird an eine zufällige Koordinate gestellt wobei 0<= x <= 10 und 0<= y <= 10
+		java.awt.Point door = new java.awt.Point();		//eine neue TÃ¼r zum Suchen wird erstellt
+		door.setLocation(rand.nextInt(10),  rand.nextInt(10));	//die TÃ¼r wird an eine zufÃ¤llige Koordinate gestellt wobei 0<= x <= 10 und 0<= y <= 10
 		
-		System.out.println("Abstand von Tür zum Spieler ist " + player.distance(door));		//der Abstand von Spieler und Tür wird ausgegeben
+		System.out.println("Abstand von TÃ¼r zum Spieler ist " + player.distance(door));		//der Abstand von Spieler und TÃ¼r wird ausgegeben
 		
 		Scanner scan = new Scanner(System.in);
 		int xplus = 0;
 		int yplus = 0;
-		while(player.equals(door) != true)	//solange der Player nicht auf der Tür steht
+		
+		while(player.equals(door) != true)	//solange der Player nicht auf der TÃ¼r steht
 		{
-			System.out.println("Bitte geben Sie auf welche x Koordinate Sie gehen möchten");
+			System.out.println("Bitte geben Sie an wie viele Schritte sie in x Richtung gehen mÃ¶chten");
 			xplus = scan.nextInt();
-			System.out.println("Bitte geben Sie auf welche y Koordinate Sie gehen möchten");
+			System.out.println("Bitte geben Sie an wie viele Schritte sie in y Richtung gehen mÃ¶chten");
 			yplus = scan.nextInt();
-			player.move(xplus, yplus);
-			if(player.equals(door) == true)	//wenn die Tür gefunden wurde wird eine Erfolgsmeldung ausgegeben
+			player.move((player.x + xplus), (player.y + yplus));
+			if(player.equals(door) == true)	//wenn die TÃ¼r gefunden wurde wird eine Erfolgsmeldung ausgegeben
 			{
-				System.out.println("Sie haben die Tür gefunden");
+				System.out.println("Sie haben die TÃ¼r gefunden");
 				break;
 			}
 			else if(player.equals(door) != true)
 			{
-				System.out.println("Sie haben die Tür nicht gefunden");
-				System.out.println("Die Tür ist noch " + player.distance(door) + "m enternt");
+				System.out.println("Sie haben die TÃ¼r nicht gefunden");
+				System.out.println("Die TÃ¼r ist noch " + player.distance(door) + "m enternt");
 				System.out.println("Ihre Aktuelle Position: " + player.getLocation());
 			}
 		}
